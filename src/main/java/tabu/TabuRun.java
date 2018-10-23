cc7e48b9bc8b6cf3087c7c4f2f04aa4a013a164c
Maciej Gruszczyński <maciek.gruszka@gmail.com>
2018-10-23 18:35:47 +0200

    alternative stop condition - count of iterations in succession without progress

--- src/main/java/tabu/TabuRun.java
+++ src/main/java/tabu/TabuRun.java
@@ -10,24 +10,33 @@ public class TabuRun {
     private static final ProblemDTO problemDTO = ProblemReader.read();
     private static final Set<TTPGenome> tabu = new HashSet<>();
     private static final int ITERATIONS_COUNT = 10000;
+    private static final int NO_PROGRESS_ITERATIONS_COUNT = 300;
 
     public static void run() {
         genetic.TTPContext.initGreedyPickingPlanStatics(problemDTO);
         Set<TTPGenome> neighbours;
+        int noProgressIterationsCounter = 0;
         TTPGenome currentBest = TTPGenome.randomInstance(problemDTO.dimension, problemDTO.items, problemDTO.capacity);
         TTPGenome overallBest = currentBest;
         tabu.add(currentBest);
-        for (int i = 0; i < ITERATIONS_COUNT; i++) {
+        for (int i = 0; stopCondition(i, noProgressIterationsCounter); i++) {
             neighbours = neighbours(currentBest);
             neighbours.removeAll(tabu);
             currentBest = best(neighbours);
             tabu.add(currentBest);
             if (currentBest.evaluate(problemDTO) > overallBest.evaluate(problemDTO)) {
                 overallBest = currentBest;
+                noProgressIterationsCounter = 0;
+            } else {
+                noProgressIterationsCounter++;
             }
             System.out.println(currentBest.evaluate(problemDTO).intValue());
-//            System.out.println("Overall best: " + overallBest.evaluate(problemDTO) + "\n");
         }
+        System.out.println("Overall best: " + overallBest.evaluate(problemDTO) + "\n");
+    }
+
+    private static boolean stopCondition(int i, int noProgressIterationsCounter) {
+        return i < ITERATIONS_COUNT && noProgressIterationsCounter < NO_PROGRESS_ITERATIONS_COUNT;
     }
 
     private static Set<TTPGenome> neighbours(TTPGenome genome) {//definicja sąsiedztwa - dokładnie 2 miasta są zamienione

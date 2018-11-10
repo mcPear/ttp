bfe63c9d8befcfe0fc1d6bf677831d9fb2b9bc4e
Maciej Gruszczyński <maciek.gruszka@gmail.com>
2018-11-10 13:13:37 +0100

    find good tabu search algorithm parameter values

--- src/main/java/tabu/TabuRun.java
+++ src/main/java/tabu/TabuRun.java
@@ -1,17 +1,18 @@
 package tabu;
 
+import genetic.Rand;
 import problem.ProblemDTO;
 import problem.ProblemReader;
 import problem.TTPGenome;
 
 import java.util.*;
 
-public class TabuRun {//todo dostrój, żeby leciał około 10 sekund, dostrój neighbours_count, weź funkcję losowego osobnika z anneal
+public class TabuRun {
     private static final ProblemDTO problemDTO = ProblemReader.read();
     private static final Set<TTPGenome> tabu = new HashSet<>();
-    private static final int ITERATIONS_COUNT = 10000;
-    private static final int NO_PROGRESS_ITERATIONS_COUNT = 1000;
-    private static final int NEIGHBOURS_COUNT = 300;
+    private static final int ITERATIONS_COUNT = 900;
+    private static final int NO_PROGRESS_ITERATIONS_COUNT = ITERATIONS_COUNT;
+    private static final int NEIGHBOURS_COUNT = 150;
 
     public static void run() {
         genetic.TTPContext.initGreedyPickingPlanStatics(problemDTO);
@@ -22,7 +23,7 @@ public class TabuRun {//todo dostrój, żeby leciał około 10 sekund, dostrój 
         tabu.add(currentBest);
 
         for (int i = 0; stopCondition(i, noProgressIterationsCounter); i++) {
-            neighbours = neighbours(currentBest); //todo tylko k sąsiadów
+            neighbours = neighbours(currentBest, NEIGHBOURS_COUNT);
             neighbours.removeAll(tabu);
             currentBest = best(neighbours);
             tabu.add(currentBest); //todo czy warto ograniczyć rozmiar i zrzucać najstarsze ?
@@ -46,17 +47,23 @@ public class TabuRun {//todo dostrój, żeby leciał około 10 sekund, dostrój 
         return currentBest.evaluate(problemDTO) > overallBest.evaluate(problemDTO);
     }
 
-    private static Set<TTPGenome> neighbours(TTPGenome genome) {//definicja sąsiedztwa - dokładnie 2 miasta są zamienione
+    private static Set<TTPGenome> neighbours(TTPGenome genome, int neighboursCount) {//definicja sąsiedztwa - dokładnie 2 miasta są zamienione
         Set<TTPGenome> neighbours = new HashSet<>();
         List<Integer> tourCopy = new ArrayList<>(genome.getTour());
-        for (int i = 0; i < tourCopy.size(); i++) {
-            for (int j = i + 1; j < tourCopy.size(); j++) {
-                if (i != j) {
-                    Collections.swap(tourCopy, i, j);
-                    neighbours.add(new TTPGenome(genome.getPickingPlan(), new ArrayList<>(tourCopy)));
-                    Collections.swap(tourCopy, i, j);
+        int i, j;
+        while (neighbours.size() < neighboursCount) {
+            i = 0;
+            j = 0;
+            while (i == j) {
+                try {
+                    i = Rand.object.nextInt(tourCopy.size());
+                    j = i + 1 + Rand.object.nextInt(tourCopy.size() - i - 1);
+                } catch (IllegalArgumentException e) {
                 }
             }
+            Collections.swap(tourCopy, i, j);
+            neighbours.add(new TTPGenome(genome.getPickingPlan(), new ArrayList<>(tourCopy)));
+            Collections.swap(tourCopy, i, j);
         }
         return neighbours;
     }
